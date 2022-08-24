9f28dce2b351df081dcf82232d58a4c3d213d812
Gaston Kennel <102702790+gatokennel@users.noreply.github.com>
2022-08-24 19:32:01 -0300

    Agrego TIENDA MODELO - Va en segunda hoja

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-
 import './App.css';
 import QuienesSomos from './components/QuienesSomos';
+import Tienda from './components/Tienda';
 import Navigation from './components/Navigation'
 import Header from './components/Header'
 function App() {
   return (
-    
     <>
-      <Navigation/>
+
+<Navigation/>
       <Header/>
-      <QuienesSomos />
+    <QuienesSomos />
+
+
+
+
+    <div className='bg-blue-800 my-5 py-3 text-center text-white text-4xl'> SEGUNDA PAGINA TIENDA ONLINE </div>
+    <Tienda />
     </>
   );
 }
