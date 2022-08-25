bea14df28d3d8ba699cfc9f1e42d832a02894094
Pablo <pablo.delapuente@gmail.com>
2022-08-25 11:20:22 -0300

    Centradas las Cards LG y arreglo del titulo que se asomaba en el final de Destacados

--- src/components/Destacados.js
+++ src/components/Destacados.js
@@ -1,4 +1,5 @@
 
+
 import Card from "./Card"
 import CardLg from "./CardLg"
 import image1 from '../assets/dcarnes.jpg'
@@ -11,7 +12,7 @@ id:1,
 title: "Nuestras Carnes",
 image: image1,
 price: 1600,
-parrafo:"'Carne de res, ternera y cordero de la más alta calidad...'"
+parrafo:"'Carnes de res, ternera y cordero de la más alta calidad...'"
 },
 {
 id:2,
@@ -19,7 +20,6 @@ title: "Mermeladas",
 image: image2,
 price: 3000,
 parrafo:"'Pulpas puras concentradas endulzadas con Stevia...'"
-
 },
 {
 id:3,
@@ -37,47 +37,51 @@ parrafo:"'transformá el sabor de tus comidas...'"
 }
 ]
 
-{/*------------------ Comienzo de lo que se va a mostrar en resoluciones menores a "lg"--------------------*/}
+/*------------------ Comienzo de lo que se va a mostrar en resoluciones menores a "lg"--------------------*/
 
 const Destacados = () => {
     return (
 
 <>
-    <div className="bg-red-800 flex justify-center lg:hidden">
-        <h1 className="text-white font-black uppercase p-4">
-        Productos destacados
-        </h1>
-    </div >
-    <div className='bg-white'>
-        <div className="h-1/2 row center space-y-0.5 bg-black rounded-lg drop-shadow-lg lg:hidden">
-        {
-        cards.map(card =>(
-            <div className="col-md-4" key={card.id}>
-            <Card title={card.title} imageSource={card.image} url={card.url} parrafo={card.parrafo}/>
-            </div>
-        ))
-        }
-    </div>
-
-    {/*------------------ Comienzo de lo que se va a mostrar en resoluciones a partir de "lg"--------------------*/}
-
-        <div className="bg-red-800 flex justify-center">
-            <h1 className="text-white font-black uppercase p-4">
-            ofertas especiales
+    <div>
+        <div className="bg-red-800 flex justify-center lg:hidden">
+            <h1 className="text-white font-black uppercase p-4 lg:hidden">
+            Productos destacados
             </h1>
-        </div>
-        <div className="hidden lg:flex xl p-5 space-x-3">
-            <div className="lg:flex xl p-5 space-x-3">
+        </div >
+        <div className='bg-white'>
+            <div className="h-1/2 row center space-y-0.5 bg-black lg:hidden">
             {
-            cards.map(card =>(
-            <div className="col-md-4" key={card.id}>
-            <CardLg title={card.title} imageSource={card.image} url={card.url} parrafo={card.parrafo} price={card.price}/>
+                cards.map(card =>(
+                    <div className="" key={card.id}>
+                    <Card title={card.title} imageSource={card.image} url={card.url} parrafo={card.parrafo}/>
+                    </div>
+                ))
+                }
             </div>
-            ))
-            }
+
+            {/*------------------ Comienzo de lo que se va a mostrar en resoluciones a partir de "lg"--------------------*/}
+
+            <div className="hidden lg:block">
+                <div className="bg-red-800 flex justify-center">
+                    <h1 className="text-white font-black uppercase p-4">
+                    ofertas especiales
+                    </h1>
+                </div>
+                <div className="flex justify-center xl p-3 space-x-3">
+                    <div className="flex xl p-5 space-x-3">
+                    {
+                    cards.map(card =>(
+                        <div className="" key={card.id}>
+                        <CardLg title={card.title} imageSource={card.image} url={card.url} parrafo={card.parrafo} price={card.price}/>
+                        </div>
+                    ))
+                    }
+                    </div>
+                </div>
             </div>
         </div>
-        </div>
+    </div>
 </>
     )
 }
