41e8b4638d52cd2ab2d3dfbf85db7178e4a7d040
Pablo <pablo.delapuente@gmail.com>
2022-09-06 10:26:20 -0300

    resuelto problema 'Card'

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,29 +1,28 @@
 
 
-
-
-const CardLg = ({title,imageSource,price,parrafo}) => {
+const Card = ({title,imageSource,parrafo}) => {
   return (
-  <figure className="w-64  bg-red-800 xl:rounded-lg drop-shadow-lg " >
-      <img className="object-cover h-[220px] xl:rounded-tl-lg xl:rounded-tr-lg" alt="Alimentos"
-        src={imageSource} />
-          <figcaption className="h-24 px-5 py-6 text-center text-xl font-semibold">
-            <h1 className="text-white">{title}</h1>
-              <p className='text-xs opacity-90 text-white'>
-              {parrafo}
-              </p>
-          </figcaption>
-              {/* <p className= "px-5 py-3 font-semibold italic lg:text-xl text-center border-t">${price} /Kg</p> */}
-              <div className="px-5 py-3 text-center ">
-                <button type="button" className= "text-white bg-[#000000] hover:bg-[#24292F]/90 focus:ring-6 focus:outline-none focus:ring-[#24292F]/30 font-medium rounded-lg text-sm px-2 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30 mr-2 mb-2 transform transition-all hover:scale-110 duration-700">
+    <figure class ="drop-shadow-lg">
+      
+        <img className = "h-40 w-screen object-cover"
+            src={imageSource} alt = "cualquiera"/>
+            <button type="button" className= "absolute bottom-0 right-2 text-white bg-[#000000] hover:bg-[#24292F]/90 focus:ring-6 focus:outline-none focus:ring-[#24292F]/30 font-medium rounded-lg text-sm px-2 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30 mr-2 mb-2 transform transition-all hover:scale-110 duration-700">
                   {/* <svg xmlns="http://www.w3.org/2000/svg" className="md:h-4 lg:h-6 w-6 px-1" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
                   </svg> */}
-                  Ver Más
+                  Ver Másy
                 </button>
-              </div>
-  </figure>
+        
+        <figcaption class="absolute bottom-0 z-90 w-2/3 h-1/2 bg-black/50 px-5 py-2 text-white">
+          
+            <h1 class="text-xl font-bold">{title}</h1>
+            <p class="mt-1 italic text-md font-light">{parrafo}</p>
+        </figcaption>
+        
+        
+    </figure>
+  )
+}
 
-  )}
 
-  export default CardLg
+export default Card
