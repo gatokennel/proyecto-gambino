a3bebc3b3083a71080384e040355192dce679781
Gaston Kennel <102702790+gatokennel@users.noreply.github.com>
2022-09-26 21:06:09 -0300

    modificaciones finales

--- src/reducers/shoppingReducer.js
+++ src/reducers/shoppingReducer.js
@@ -4,18 +4,29 @@ import { TYPES } from "../actions/shoppingAction"
 
 export const shoppingInitialState ={
     products:[
-        {id:1,name:"Producto1",price:100},
-        {id:2,name:"Producto2",price:200},
-        {id:3,name:"Producto3",price:300},
-        {id:4,name:"Producto4",price:1300},
-        {id:5,name:"Producto5",price:500},
-        {id:6,name:"Producto6",price:600}
-    ],
+    //     {id:1,name:"Producto1",price:100},
+    //     {id:2,name:"Producto2",price:200},
+    //     {id:3,name:"Producto3",price:300},
+    //     {id:4,name:"Producto4",price:1300},
+    //     {id:5,name:"Producto5",price:500},
+    //     {id:6,name:"Producto6",price:600}
+     ],
     cart:[]
 }
 export function shoppingReducer(state,action) {
     switch (action.type) {
+        case TYPES.READ_STATE: {
+            return {
+                ...state,
+                    products: action.payload[0],
+                    cart: action.payload[1]
+            }
+        }
+
         case TYPES.ADD_TO_CART:{
+
+
+
         let newItem = state.products.find(
             product => product.id === action.payload
             );   
